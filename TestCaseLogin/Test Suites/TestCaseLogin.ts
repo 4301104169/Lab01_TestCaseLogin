cd88804658b047d1918b0829ae0fd77a57aeaa74
4301104169 <baothinh2405@gmail.com>
2020-09-24 13:38:37 +0700

    First_Push

---
+++ TestCaseLogin/Test Suites/TestCaseLogin.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestCaseLogin</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>4ff4471c-2781-433f-b116-f5e05b9d4d32</testSuiteGuid>
+   <testCaseLink>
+      <guid>d2ba0338-d414-40fa-a718-d8a99bc90c6e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Check login with all empty fields</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2ba344f6-72e0-4e6b-97e8-f499f3bff109</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Check login with correct Username and Password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0088b0bd-3030-40fb-b612-23ed8c30c547</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Check login with correct Username and wrong Password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ba5cc86f-623c-4f53-96a1-532f19392722</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Check login with wrong Username and correct Password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>885df464-6402-4a03-8745-6ec144fe38b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Check login with wrong Username and Password</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
